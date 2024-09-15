a7a4f081307a74b1fe0042e776684ae691ae64c8
Bai1eyCh <164561780+Bai1eyCh@users.noreply.github.com>
2024-09-15 20:54:44 +0100

    Homework

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-document.addEventListener("DOMContentLoaded",() => {
-
+document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
 
-    for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then(data => {
-            image.src = data.message
-            image.width = 300;
-            image.height = 300;
-        })
+    for (const image of images) {
+        fetch("https://picsum.photos/500/300")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok.');
+                }
+                return response.blob(); // Get the image as a blob
+            })
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                image.src = url;
+                image.width = 500; // Set the width of the image
+                image.height = 300; // Set the height of the image
+            })
+            .catch(error => console.error('There was a problem with the fetch operation:', error));
     }
-
-})
+});
